b15eed483177b79dc01a0141f8521935a3699781
fimercadog <fimercadog@gamil.com>
2021-01-27 01:06:24 -0500

    controlador_procedimiento_modificar.php, modelo procedimiento_modificar.php, procedimiento_modificar().js

--- js/procedimiento.js
+++ js/procedimiento.js
@@ -1,5 +1,7 @@
+var tableprocedimiento;
+
 function listar_procedimiento() {
-    var tableprocedimiento = $("#tabla_procedimiento").DataTable({
+    tableprocedimiento = $("#tabla_procedimiento").DataTable({
         "ordering": false,
         "bLengthChange": false,
         "searching": { "regex": false },
@@ -55,10 +57,17 @@ function listar_procedimiento() {
 
 }
 
+function filterGlobal() {
+    $('#tabla_procedimiento').DataTable().search(
+        $('#global_filter').val(),
+    ).draw();
+}
+
 function AbrirModalRegistro() {
     $("#modal_registro").modal({ backdrop: 'static', keyboard: false })
     $("#modal_registro").modal('show');
 }
+
 
 function Registro_Procedimiento() {
     var procedimiento = $("#txt_procedimiento").val();
@@ -89,7 +98,59 @@ function Registro_Procedimiento() {
     })
 }
 
-
 function LimpiarDatos() {
     $("#txt_procedimiento").val("");
+}
+$('#tabla_procedimiento').on('click', '.editar', function() {
+    var data = tableprocedimiento.row($(this).parents('tr')).data(); //Detecta a que fila hago click y me captura los datos en la variable data.
+    if (tableprocedimiento.row(this).child.isShown()) { //Cuando esta en tamaño responsivo
+        var data = tableprocedimiento.row(this).data();
+    }
+    $("#modal_editar").modal({ backdrop: 'static', keyboard: false })
+    $("#modal_editar").modal('show');
+    $("#txt_idprocedimiento").val(data.procedimiento_id);
+    $("#txt_procedimiento_actual_editar").val(data.procedimiento_nombre);
+    $("#txt_procedimiento_nuevo_editar").val(data.procedimiento_nombre);
+    $("#cbm_estatus_editar").val(data.procedimiento_estatus).trigger("change");
+})
+
+function Modificar_Procedimiento() {
+    //Vamos a llevar el actual y el nuevo, para luego en nuestro procedure hacer una condicional
+    // Si el procedimiento actual == nuevo, entonces que me actualize el estatus, si es diferente 
+    // que me busque en la bd si existe o no, para votar un msj que esta repitdo si existo y si no me pasa a registrar.
+
+    var id = $("#txt_idprocedimiento").val();
+    var procedimientoactual = $("#txt_procedimiento_actual_editar").val();
+    var procedimientonuevo = $("#txt_procedimiento_nuevo_editar").val();
+    var estatus = $("#cbm_estatus_editar").val();
+    if (id.length == 0) {
+        return Swal.fire("Mensaje De Advertencia", "El id del campo esta vacio", "warning");
+    }
+
+    if (procedimientonuevo.length == 0) {
+        return Swal.fire("Mensaje De Advertencia", "Debe ingresar un procedimiento", "warning");
+    }
+
+    $.ajax({
+        url: '../controlador/procedimiento/controlador_procedimiento_modificar.php',
+        type: 'POST',
+        data: {
+            id: id,
+            procedimientoactual: procedimientoactual,
+            procedimientonuevo: procedimientonuevo,
+            estatus: estatus
+        }
+    }).done(function(resp) {
+        if (resp > 0) {
+            $("#modal_editar").modal('hide');
+            if (resp == 1) {
+                listar_procedimiento();
+                Swal.fire("Mensaje De Confirmacion", "Datos actualizados correctamente", "success");
+            } else {
+                Swal.fire("Mensaje De Advertencia", "El procedimiento ya se encuentra en nuestra base de datos", "warning");
+            }
+        } else {
+            Swal.fire("Mensaje De ERROR", "Lo sentimos no se pudo completar su actualizacion", "error");
+        }
+    })
 }
--- modelo/modelo_procedimiento.php
+++ modelo/modelo_procedimiento.php
@@ -22,9 +22,21 @@ class Modelo_Procedimiento
         }
     }
 
+
     function Registrar_Procedimiento($procedimiento, $estatus)
     {
         $sql = "call SP_REGISTRAR_PROCEDIMIENTO('$procedimiento','$estatus')";
+        if ($consulta = $this->conexion->conexion->query($sql)) {
+            if ($row = mysqli_fetch_array($consulta)) {
+                return $id = trim($row[0]); //retorna valores
+            }
+            $this->conexion->cerrar();
+        }
+    }
+
+    function Modificar_Procedimiento($id, $procedimientoactual, $procedimientonuevo, $estatus)
+    {
+        $sql = "call SP_MODIFICAR_PROCEDIMIENTO('$id','$procedimientoactual','$procedimientonuevo','$estatus')";
         if ($consulta = $this->conexion->conexion->query($sql)) {
             if ($row = mysqli_fetch_array($consulta)) {
                 return $id = trim($row[0]); //retorna valores
